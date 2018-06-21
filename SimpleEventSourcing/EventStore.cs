b21e7f43025370b453a5b5702478f7ec6baad8c0
David Bottiau <dbottiau@mobilis-afm.com>
2018-06-21 15:10:22 +0200

    chore: create first version

---
+++ SimpleEventSourcing/EventStore.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+
+namespace SimpleEventSourcing
+{
+    public abstract class EventStore
+    {
+        private readonly Subject<object> _eventSubject = new Subject<object>();
+
+        public virtual void Dispatch(object @event)
+        {
+            _eventSubject.OnNext(@event);
+        }
+
+        public IObservable<object> ObserveEvent()
+        {
+            return _eventSubject;
+        }
+        public IObservable<T> ObserveEvent<T>() where T : class
+        {
+            return _eventSubject.OfType<T>();
+        }
+    }
+}
---
+++ SimpleEventSourcing/EventView.cs
@@ -0,0 +1,24 @@
+﻿using System.Reactive.Subjects;
+
+namespace SimpleEventSourcing
+{
+    public abstract class EventView<TState> where TState : class, new()
+    {
+        private readonly Subject<TState> _stateSubject = new Subject<TState>();
+
+        public TState State { get; private set; }
+
+        protected EventView(TState initialState = null)
+        {
+            State = initialState ?? new TState();
+        }
+
+        public void Handle(object @event)
+        {
+            State = Execute(State, @event);
+            _stateSubject.OnNext(State);
+        }
+
+        public abstract TState Execute(TState state, object @event);
+    }
+}
