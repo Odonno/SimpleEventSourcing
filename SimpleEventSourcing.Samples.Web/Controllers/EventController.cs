d8b51603646fe9c95f19e75a7bd201285542bd25
Odonno <bottiau.david@laposte.net>
2018-11-18 14:07:39 +0100

    fix(samples): reset auto increment in database when replaying events

--- SimpleEventSourcing.Samples.Web/Controllers/EventController.cs
+++ SimpleEventSourcing.Samples.Web/Controllers/EventController.cs
@@ -81,6 +81,7 @@ namespace SimpleEventSourcing.Samples.Web.Controllers
                     DELETE FROM [Cart];
                     DELETE FROM [Order];
                     DELETE FROM [Item];
+                    DELETE FROM [sqlite_sequence];
                     "
                 );
             }
